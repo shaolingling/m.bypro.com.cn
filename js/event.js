39c98a3fbda2cdea86df52e1d8f765ee1642daa0
v8xm <448534841@qq.com>
2016-05-13 18:02:45 +0800

    no message

--- js/event.js
+++ js/event.js
@@ -1,9 +1,28 @@
 /**
  * Created by ximing on 2016/5/13.
  */
-$.init();
 
-$(document).on('click','.alert-text',function () {
-    $.alert('请先登录');
-    window.location.href = "./sign.html";
+
+
+$(document).on("pageInit", function(e, pageId, $page) {
+    $.init();
+    $(document).on('click','.alert-text',function () {
+        $.alert('请先登录');
+        window.location.href = "./sign.html";
+    });
+
+    if(pageId == "details") {
+
+
+        var imgWidth = $("body").width();
+        $("#swiper img").height(imgWidth);
+        $("#swiper img").width(imgWidth);
+
+
+        //baidu map api init
+        var mp = new BMap.Map('map');
+        mp.centerAndZoom(new BMap.Point(121.491, 31.233), 11);
+
+    }
 });
+
